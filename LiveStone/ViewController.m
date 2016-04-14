8bfacd3aee69eeff41e51a9ff5e7335d2e8a6b8f
Cocos <543314602@qq.com>
2016-04-15 07:46:58 +0800

    Initial Commit

---
+++ LiveStone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LiveStone
+//
+//  Created by 郑克明 on 16/4/15.
+//  Copyright © 2016年 Cocos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
