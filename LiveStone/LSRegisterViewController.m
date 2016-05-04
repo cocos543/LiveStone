5f36bdc9dbfb1308f05cb8c45ee0ae1085908e82
Cocos <543314602@qq.com>
2016-05-04 22:47:29 +0800

    1.完善登录界面

--- LiveStone/LSRegisterViewController.m
+++ LiveStone/LSRegisterViewController.m
@@ -23,13 +23,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCancel target:self action:@selector(cancel:)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"取消" style:UIBarButtonItemStylePlain target:self action:@selector(cancel:)];
     self.title = @"登录";
     [self adjustScrollView];
 }
 
--(void)viewWillAppear:(BOOL)animated{
+- (void)viewWillAppear:(BOOL)animated{
     self.navigationController.navigationBar.barTintColor = [CCSimpleTools stringToColor:NAVIGATIONBAR_BACKGROUND_COLOR opacity:1];
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
     self.navigationController.navigationBar.translucent = NO;
     self.extendedLayoutIncludesOpaqueBars = YES;
 }
@@ -39,12 +41,36 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)adjustScrollView{
+- (void)viewDidLayoutSubviews{
+    [self drawInputView];
+}
+
+- (void)adjustScrollView{
     self.loginBtnBottomConstraint.constant  = SCREEN_HEIGHT - (self.loginButton.frame.origin.y + self.loginButton.frame.size.height) + 0.5;
     self.automaticallyAdjustsScrollViewInsets = NO;
     UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(backgroundTapped:)];
     [self.scrollView addGestureRecognizer:tapGesture];
     
+}
+
+- (void)drawInputView{
+    static CAShapeLayer *layer = nil;
+    if (!layer) {
+        layer = [[CAShapeLayer alloc] init];
+    }else{
+        [layer removeFromSuperlayer];
+    }
+    layer.strokeColor = [CCSimpleTools stringToColor:LIVESTONE_GRAY_COLOR opacity:1].CGColor;
+    layer.fillColor = [UIColor clearColor].CGColor;
+    layer.lineWidth = 1;
+    UIBezierPath *path = [UIBezierPath bezierPath];
+    CGRect frame = self.inputView.frame;
+    [path moveToPoint:CGPointMake(0, frame.size.height / 2)];
+    [path addLineToPoint:CGPointMake(frame.size.width, frame.size.height / 2)];
+    layer.path = path.CGPath;
+    
+    [self.inputView.layer addSublayer:layer];
+    self.inputView.layer.cornerRadius = 10;
 }
 
 - (void)backgroundTapped:(id)sender {
@@ -61,6 +87,15 @@
 - (void)cancel:(id)sender{
     [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (IBAction)joinUsAction:(id)sender {
+    [UIView animateWithDuration:3 animations:^{
+        self.inputView.transform = CGAffineTransformMakeScale(0.5, 0.5);
+    } completion:^(BOOL finished) {
+    }];
+}
+
+
 /*
 #pragma mark - Navigation
 
