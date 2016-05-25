bf2dc1e3700cc17ca23bbdf0122e547746421f05
Cocos <543314602@qq.com>
2016-05-25 11:31:01 +0800

    1. 新增发布代祷请求对象. 2. 完成发布代祷功能. 3. 将LSIntercessionService从单例调正为非单例,方便多个控制器将LSIntercessionService的代理设置为自己. 4. 在LSServiceBase中新增NotLogin协议方法

---
+++ LiveStone/LSIntercessionPublishRequestItem.h
@@ -0,0 +1,13 @@
+//
+//  LSIntercessionPublishRequestItem.h
+//  LiveStone
+//
+//  Created by 郑克明 on 16/5/25.
+//  Copyright © 2016年 Cocos. All rights reserved.
+//
+
+#import "LSBaseModel.h"
+
+@interface LSIntercessionPublishRequestItem : LSBaseModel
+
+@end
---
+++ LiveStone/LSIntercessionPublishRequestItem.m
@@ -0,0 +1,13 @@
+//
+//  LSIntercessionPublishRequestItem.m
+//  LiveStone
+//
+//  Created by 郑克明 on 16/5/25.
+//  Copyright © 2016年 Cocos. All rights reserved.
+//
+
+#import "LSIntercessionPublishRequestItem.h"
+
+@implementation LSIntercessionPublishRequestItem
+
+@end
